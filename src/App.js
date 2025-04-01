18611183733bbc40599eeefeef1a583edfd73bdf
Jeyaram2003 <148937393+Jeyaram2003@users.noreply.github.com>
2025-04-01 12:18:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import questions from './questions';
+import {useState} from "react";
+function App(){
+  const [questionIndex, setQuestionIndex]=useState(0);
+  const [score, setScore]=useState(0);
+  const [results, setResults]=useState(false);
+  const replay=()=>
+  {
+    setQuestionIndex(0);
+    setScore(0);
+    setResults(false);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const present_question=questions[questionIndex];
+  const get_Choice=(index)=>{
+    if(present_question.answer===index)
+    {
+      setScore(score+1);
+    }
+    const next_question=questionIndex+1;
+    if(next_question<questions.length)
+    {
+      setQuestionIndex(questionIndex +1);
+    }
+    else{
+      setResults(true);
+    }
+  };
+  return(
+      <div className='quiz_box'>
+        {results?(<>
+        <h1>{score}</h1><br/>
+        <button onClick={replay}>Play again</button>
+        </>):
+        (<div className='quiz_question'>
+          {present_question.question}
+          
+          <div className='quiz_box_options'></div>
+          <ul className='quiz_ul'>
+            {present_question.options.map
+            ((option,i)=>
+            {return<li className='quiz_li' onClick={
+              ()=>get_Choice(i)}>{option}</li>
+            })}
+            
+            </ul>
+      </div>)}
+      </div>
   );
 }
 
